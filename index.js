b2596b65036f43b4d16db9391a395274c24adf9a
HamletMYan <hammkrtchyan7@gmail.com>
2024-04-23 18:56:15 +0400

    add console.log

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
-const multer  = require('multer')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'))
 const router = require("./router/index");
 
-
+console.log(34343434);
 app.use("/", router);
 
 app.listen(8080);
